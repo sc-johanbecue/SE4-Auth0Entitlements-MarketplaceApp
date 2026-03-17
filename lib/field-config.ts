cb09ab21104282dce28df2c81e598b23ffb7792b
sc-johanbecue <johan.becue@sitecore.com>
2026-03-17 14:51:35 +0100

    modified:   lib/field-config.ts

--- lib/field-config.ts
+++ lib/field-config.ts
@@ -5,14 +5,17 @@
 
 export const ENTITLEMENTS_FIELD_NAME = "Entitlements";
 export const ENTITLEMENTS_SOURCE_PATH =
+  process.env.NEXT_PUBLIC_ENTITLEMENTS_SOURCE_PATH ||
   "/sitecore/content/adp-digital-web-assets/ADP Portal/Data/Entitlements";
 
 export const ENTITLEMENTS_OPERATOR_FIELD_NAME = "EntitlementsOperator";
 export const OPERATORS_SOURCE_PATH =
+  process.env.NEXT_PUBLIC_OPERATORS_SOURCE_PATH ||
   "/sitecore/content/adp-digital-web-assets/ADP Portal/Data/Operators";
 
 export const ROLES_FIELD_NAME = "Roles";
 export const ROLES_SOURCE_PATH =
+  process.env.NEXT_PUBLIC_ROLES_SOURCE_PATH ||
   "/sitecore/content/adp-digital-web-assets/ADP Portal/Data/Roles";
 
 export const ROLES_OPERATOR_FIELD_NAME = "RolesOperator";
