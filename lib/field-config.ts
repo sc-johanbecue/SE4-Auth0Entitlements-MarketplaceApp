1cdc155a7ad8097782e686f17029250334bed82b
sitecore-jbe <jbe@sitecore.net>
2026-08-03 04:03:47 +0200

    modified:   lib/field-config.ts

--- lib/field-config.ts
+++ lib/field-config.ts
@@ -6,16 +6,16 @@
 export const ENTITLEMENTS_FIELD_NAME = "Entitlements";
 export const ENTITLEMENTS_SOURCE_PATH =
   process.env.NEXT_PUBLIC_ENTITLEMENTS_SOURCE_PATH ||
-  "/sitecore/content/malvern-panalytical/malvern-panalytical/Data/Entitlements";
+  "/sitecore/content/british-council/british-council/Data/Entitlements";
 
 export const ENTITLEMENTS_OPERATOR_FIELD_NAME = "EntitlementsOperator";
 export const OPERATORS_SOURCE_PATH =
   process.env.NEXT_PUBLIC_OPERATORS_SOURCE_PATH ||
-  "/sitecore/content/malvern-panalytical/malvern-panalytical/Data/Operators";
+  "/sitecore/content/british-council/british-council/Data/Operators";
 
 export const ROLES_FIELD_NAME = "Roles";
 export const ROLES_SOURCE_PATH =
   process.env.NEXT_PUBLIC_ROLES_SOURCE_PATH ||
-  "/sitecore/content/malvern-panalytical/malvern-panalytical/Data/Roles";
+  "/sitecore/content/british-council/british-council/Data/Roles";
 
 export const ROLES_OPERATOR_FIELD_NAME = "RolesOperator";
